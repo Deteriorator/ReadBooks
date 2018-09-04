88e7b4995bde31f826df4576d07f2d90ca506177
Deteriorator <Liangz.org@gmail.com>
2018-09-04 18:20:06 +0800

    创建空文本文件

--- 编程小白的第一本Python入门书/CreatFilesPy3.py
+++ 编程小白的第一本Python入门书/CreatFilesPy3.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name：     CreatFiles
-   Description :   快速创建文本
+   File Name：     CreatFilesPy3
+   Description :   快速创建空文本
    Author :        Liangz
    date：          2018/8/4
 -------------------------------------------------
    Change Activity:
-                   2018/8/4:
+                   2018/9/4:
 -------------------------------------------------
 """
 __author__ = 'Liangz'
@@ -22,7 +22,7 @@ def create_file(num=1):
 
 
 def main():
-    parse = optparse.OptionParser(u'使用方法 python CreateFiles.py -n <num>')
+    parse = optparse.OptionParser(u'使用方法 python CreateFilesPy3.py -n <num>')
     parse.add_option('-n', dest='num', type='int', help=u'创建文件的数量')
     (options, args) = parse.parse_args()
     num = options.num
