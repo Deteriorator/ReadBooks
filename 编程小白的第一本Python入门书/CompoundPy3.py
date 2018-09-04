6cbd708d1c0bc27a5d2d5ac645e684c150f01336
Deteriorator <Liangz.org@gmail.com>
2018-09-04 18:18:48 +0800

    复利计算

--- 编程小白的第一本Python入门书/CompoundPy3.py
+++ 编程小白的第一本Python入门书/CompoundPy3.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name：     Compound
+   File Name：     CompoundPy3
    Description :   复利计算函数
    Author :        Liangz
    date：          2018/8/4
 -------------------------------------------------
    Change Activity:
-                   2018/8/4:
+                   2018/9/4:
 -------------------------------------------------
 """
 __author__ = 'Liangz'
@@ -24,7 +24,7 @@ def invest(amount, rate=0.05, time=1):
 
 
 def main():
-    parse = optparse.OptionParser(u'使用方法 python Compound.py -a <amount> -r <rate> -t <time>')
+    parse = optparse.OptionParser(u'使用方法 python CompoundPy3.py -a <amount> -r <rate> -t <time>')
     parse.add_option('-a', dest='amount', type='float', help=u'本金')
     parse.add_option('-r', dest='rate', type='float', help=u'利率，可以是小数')
     parse.add_option('-t', dest='time', type='int', help=u'时间/年，为整数')
