b2c132d55f41fac5aaed946265c7ff51a05268dc
Deteriorator <Liangz.org@gmail.com>
2018-09-30 18:01:47 +0800

    Alien-Invasion

--- Python编程：从入门到实践/AlienInvasion/Alien-Invasion.py
+++ Python编程：从入门到实践/AlienInvasion/Alien-Invasion.py
@@ -14,19 +14,30 @@ __author__ = 'Liangz'
 
 import sys
 import pygame
-
+from settings import Settings
 
 def run_game():
+    # 初始化Pygame、设置和屏幕对象
+
+    game_settings = Settings()
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
+
     pygame.display.set_caption("Alien Invasion")
+
+    # 设置背景色
+    # bg_color = (230,230,230)
 
     # 开始游戏主循环
     while True :
+
         # 监控键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环时都重回屏幕
+        screen.fill(game_settings.bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
