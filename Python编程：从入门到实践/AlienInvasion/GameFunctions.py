ae190cebd85a055fe0e6af7b4872a31421bce5c3
Deteriorator <Liangz.org@gmail.com>
2018-10-09 16:53:39 +0800

    rebuild

--- Python编程：从入门到实践/AlienInvasion/GameFunctions.py
+++ Python编程：从入门到实践/AlienInvasion/GameFunctions.py
@@ -16,6 +16,22 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def chech_events(ship):
     """响应按键和鼠标事件"""
     for event in pygame.event.get():
@@ -23,18 +39,20 @@ def chech_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # 向右移动飞船
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                # 向左移动飞船
-                ship.moving_left = True
+            # if event.key == pygame.K_RIGHT:
+            #     # 向右移动飞船
+            #     ship.moving_right = True
+            # elif event.key == pygame.K_LEFT:
+            #     # 向左移动飞船
+            #     ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            # if event.key == pygame.K_RIGHT:
+            #     ship.moving_right = False
+            # elif event.key == pygame.K_LEFT:
+            #     ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(game_setting, screen, ship):
