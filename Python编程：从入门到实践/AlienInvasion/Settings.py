f7428b57a00c49f8b2f90c48eec7da16de990cc1
Deteriorator <Liangz.org@gmail.com>
2018-10-09 17:26:11 +0800

    Bullet

--- Python编程：从入门到实践/AlienInvasion/Settings.py
+++ Python编程：从入门到实践/AlienInvasion/Settings.py
@@ -27,3 +27,8 @@ class Settings():
         # 飞船设置
         self.ship_speed_factor = 1.5
 
+        # 子弹
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
