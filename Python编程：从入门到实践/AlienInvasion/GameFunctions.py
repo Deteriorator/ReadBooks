c55546153f05f9e41637df4127790f0900e162e6
Deteriorator <Liangz.org@gmail.com>
2018-10-10 15:57:15 +0800

    fire_bullets

--- Python编程：从入门到实践/AlienInvasion/GameFunctions.py
+++ Python编程：从入门到实践/AlienInvasion/GameFunctions.py
@@ -23,10 +23,11 @@ def check_keydown_events(event, game_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # 创建一颗子弹，并将其加入到编组bullets中
-        if len(bullets) < game_settings.bullets_allowed:
-            new_bullet = Bullet(game_settings, screen, ship)
-            bullets.add(new_bullet)
+        # # 创建一颗子弹，并将其加入到编组bullets中
+        # if len(bullets) < game_settings.bullets_allowed:
+        #     new_bullet = Bullet(game_settings, screen, ship)
+        #     bullets.add(new_bullet)
+        fire_bullet(game_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -84,3 +85,11 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+
+
+def fire_bullet(game_settings, screen, ship, bullets):
+    """如果还没有达到限制，就发射一颗子弹"""
+    # 创建新子弹，并将其加入到编组bullets中
+    if len(bullets) < game_settings.bullets_allowed:
+        new_bullet = Bullet(game_settings, screen, ship)
+        bullets.add(new_bullet)
