5f8e34e112e8b2eed5edf6e907e7eebef257cc06
Deteriorator <Liangz.org@gmail.com>
2018-10-15 20:05:24 +0800

    add score

--- Python编程：从入门到实践/AlienInvasion/GameStatus.py
+++ Python编程：从入门到实践/AlienInvasion/GameStatus.py
@@ -29,3 +29,4 @@ class GameStatus():
     def reset_status(self):
         """初始化在游戏运行期间可能变化的统计信息"""
         self.ships_left = self.game_settings.ship_limit
+        self.score = 0
---
+++ Python编程：从入门到实践/AlienInvasion/ScoreBoard.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     ScoreBoard.py
+   Description :
+   Author :        Liangz
+   Date：          2018/10/15
+-------------------------------------------------
+   Change Activity:
+                   2018/10/15:
+-------------------------------------------------
+"""
+__author__ = 'Liangz'
+
+import pygame.font
+
+class ScoreBoard():
+    """显示得分信息的类"""
+
+    def __init__(self, game_settings, screen, status):
+        """初始化显示得分涉及的属性"""
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.game_settings = game_settings
+        self.status = status
+
+        # 显示得分信息时使用的字体设置
+        self.text_color = (30, 30, 30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # 准备初始得分图像
+        self.prep_score()
+
+    def prep_score(self):
+        """将得放装换为一幅渲染的图像"""
+        score_str = str(self.status.score)
+        
