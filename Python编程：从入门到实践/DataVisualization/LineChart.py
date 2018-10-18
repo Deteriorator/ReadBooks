e7cf025000c28ca6590ce6f2fe9e0327483de855
Deteriorator <Liangz.org@gmail.com>
2018-10-18 19:50:03 +0800

    DataVisualization

---
+++ Python编程：从入门到实践/DataVisualization/LineChart.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     LineChart
+   Description :
+   Author :        Liangz
+   Date：          2018/10/18
+-------------------------------------------------
+   Change Activity:
+                   2018/10/18:
+-------------------------------------------------
+"""
+__author__ = 'Liangz'
+
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()
