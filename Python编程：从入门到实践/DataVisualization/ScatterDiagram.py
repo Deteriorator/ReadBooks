1f3bcc4db0720ac6f59b1160c75878054a190d95
Deteriorator <Liangz.org@gmail.com>
2018-10-22 16:25:30 +0800

    scatter diagram

---
+++ Python编程：从入门到实践/DataVisualization/ScatterDiagram.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     ScatterDiagram
+   Description :   散点图
+   Author :        Liangz
+   Date：          2018/10/22
+-------------------------------------------------
+   Change Activity:
+                   2018/10/22:
+-------------------------------------------------
+"""
+__author__ = 'Liangz'
+
+import matplotlib.pyplot as plt
+
+plt.scatter(2, 4)
+plt.show()
+
