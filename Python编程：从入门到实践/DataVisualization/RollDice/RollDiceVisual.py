097669af2de82638ef9969017fe001374f16480f
Deteriorator <Liangz.org@gmail.com>
2018-10-24 16:05:08 +0800

    analyze num frequency

--- Python编程：从入门到实践/DataVisualization/RollDice/RollDiceVisual.py
+++ Python编程：从入门到实践/DataVisualization/RollDice/RollDiceVisual.py
@@ -21,8 +21,14 @@ dice = RollDice()
 
 # 掷几次骰子，并将结果存储在一个列表中
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = dice.roll()
     results.append(result)
 
-print(results)
+# 分析结果
+frequencies = []
+for value in range(1, dice.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
