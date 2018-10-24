4eb6cf923aae1c748c81ba70d94f17f50162fc50
Deteriorator <Liangz.org@gmail.com>
2018-10-24 16:13:57 +0800

    可视化

--- Python编程：从入门到实践/DataVisualization/RollDice/RollDiceVisual.py
+++ Python编程：从入门到实践/DataVisualization/RollDice/RollDiceVisual.py
@@ -14,6 +14,7 @@ __author__ = 'Liangz'
 
 
 from RollDice import RollDice
+import pygal
 
 
 # 创建一个D6
@@ -30,5 +31,16 @@ frequencies = []
 for value in range(1, dice.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = 'Results of rolling one D6 1000 Times.'
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "结果频率"
+
+hist.add('D6', frequencies)
+hist.render_to_file('RollDiceVisual.svg')
 
 print(frequencies)
