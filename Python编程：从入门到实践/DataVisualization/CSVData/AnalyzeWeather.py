4cf3afa1e7852c6f59480a9e66c1acebc840e818
Deteriorator <Liangz.org@gmail.com>
2018-10-24 17:05:15 +0800

    make csv more easier to understand

--- Python编程：从入门到实践/DataVisualization/CSVData/AnalyzeWeather.py
+++ Python编程：从入门到实践/DataVisualization/CSVData/AnalyzeWeather.py
@@ -20,5 +20,7 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as file:
     reader = csv.reader(file)
     header_row = next(reader)
-    print(header_row)
+    # print(header_row)
 
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
