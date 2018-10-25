2f12fba207ce8385a00c6ad4b5f7f8f084aa1fdd
Deteriorator <Liangz.org@gmail.com>
2018-10-25 22:40:15 +0800

    get country code

---
+++ Python编程：从入门到实践/DataVisualization/PopulationMap/CountryCode.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     CountryCode
+   Description :
+   Author :        Liangz
+   Date：          2018/10/25
+-------------------------------------------------
+   Change Activity:
+                   2018/10/25:
+-------------------------------------------------
+"""
+__author__ = 'Liangz'
+
+
+from pygal_maps_world.i18n import COUNTRIES
+
+
+def get_country_code(country_name):
+
+
