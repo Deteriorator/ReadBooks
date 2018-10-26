bf66fef457c0d98b5c81f80ef698b2413a5b81ce
Deteriorator <Liangz.org@gmail.com>
2018-10-26 17:02:57 +0800

    three level

--- Python编程：从入门到实践/DataVisualization/PopulationMap/WorldPopulation.py
+++ Python编程：从入门到实践/DataVisualization/PopulationMap/WorldPopulation.py
@@ -37,8 +37,22 @@ for pop_dict in pop_data:
         # else:
         #     print('Error - ' + country_name)
 
+# 根据人口数量将所有的国家分成三组
+world_population_1, world_population_2, world_population_3 = {}, {}, {}
+for world_code, population in whole_population.items():
+    if population < 10000000:
+        world_population_1[world_code] = population
+    elif population < 1000000000:
+        world_population_2[world_code] = population
+    else:
+        world_population_3[world_code] = population
+
+# 看看每组分别包含多少个国家
+print(len(world_population_1), len(world_population_2), len(world_population_3))
+
 world_map = World()
 world_map.title = 'World Population In 2010, By Country'
-world_map.add('2010', whole_population)
-
-world_map.render_to_file('world_population.svg')
+world_map.add('0 - 10M', world_population_1)
+world_map.add('10M - 1BN', world_population_2)
+world_map.add('>1BN', world_population_3)
+world_map.render_to_file('threelevel.svg')
