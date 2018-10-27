77524d3477280e066edeee15b89a55f913a6bf7f
Deteriorator <Liangz.org@gmail.com>
2018-10-27 17:43:58 +0800

    print info

--- Python编程：从入门到实践/DataVisualization/GithubAPI/GithubAPI.py
+++ Python编程：从入门到实践/DataVisualization/GithubAPI/GithubAPI.py
@@ -30,19 +30,19 @@ repo_dicts = response_dict['items']
 print('Repositories Returned:', len(repo_dicts))
 
 # 研究第一个仓库
-repo_dict = repo_dicts[0]
-print('\nKeys', len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+# repo_dict = repo_dicts[0]
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    # print(key)
 
-print("\nSelected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Stars:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('Updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+    print("\nSelected information about first repository:")
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Created:', repo_dict['created_at'])
+    print('Updated:', repo_dict['updated_at'])
+    print('Description:', repo_dict['description'])
 
 # 处理结果
 # print(response_dict.keys())
