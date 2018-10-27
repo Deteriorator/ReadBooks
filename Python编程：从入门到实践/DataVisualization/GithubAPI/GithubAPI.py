8b4a57a6d331a24c607ce4da6e9a7c1cc1fcf7ba
Deteriorator <Liangz.org@gmail.com>
2018-10-27 17:36:06 +0800

    get some info

--- Python编程：从入门到实践/DataVisualization/GithubAPI/GithubAPI.py
+++ Python编程：从入门到实践/DataVisualization/GithubAPI/GithubAPI.py
@@ -23,6 +23,26 @@ print('Status Code:', r.status_code)
 
 # 将API响应存储到一个变量中
 response_dict = r.json()
+print('Total Repositories:', response_dict['total_count'])
+
+# 探索有关仓库的信息
+repo_dicts = response_dict['items']
+print('Repositories Returned:', len(repo_dicts))
+
+# 研究第一个仓库
+repo_dict = repo_dicts[0]
+print('\nKeys', len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+print("\nSelected information about first repository:")
+print('Name:', repo_dict['name'])
+print('Owner:', repo_dict['owner']['login'])
+print('Stars:', repo_dict['stargazers_count'])
+print('Repository:', repo_dict['html_url'])
+print('Created:', repo_dict['created_at'])
+print('Updated:', repo_dict['updated_at'])
+print('Description:', repo_dict['description'])
 
 # 处理结果
-print(response_dict.keys())
+# print(response_dict.keys())
