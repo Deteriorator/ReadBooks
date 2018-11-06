a87938c701fc0fb72cbb1d8f3e72ceebce4c072f
Deteriorator <Liangz.org@gmail.com>
2018-11-06 17:53:07 +0800

    flash message

--- FlaskWeb/Hello.py
+++ FlaskWeb/Hello.py
@@ -13,7 +13,7 @@
 __author__ = 'Liangz'
 
 
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -40,6 +40,9 @@ def index():
     # name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))  # current_time=datetime.utcnow())
