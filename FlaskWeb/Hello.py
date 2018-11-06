cfc81ebc3eca6d6d1dac34ccfc23cb431c4f55e2
Deteriorator <Liangz.org@gmail.com>
2018-11-06 15:52:44 +0800

    study scripts block

--- FlaskWeb/Hello.py
+++ FlaskWeb/Hello.py
@@ -12,16 +12,22 @@
 """
 __author__ = 'Liangz'
 
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from datetime import datetime
+# from flask.ext.bootstrap import Bootstrap
+# from flask.ext.moment import Moment
 app = Flask(__name__)
+moment = Moment(app)
 bootstrap = Bootstrap(app)
+
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
