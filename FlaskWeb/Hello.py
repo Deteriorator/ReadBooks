c1704103a2b967fabbcda152e2fec0ea269ea725
Deteriorator <Liangz.org@gmail.com>
2018-11-07 16:50:47 +0800

    SQLAlchemy

--- FlaskWeb/Hello.py
+++ FlaskWeb/Hello.py
@@ -12,21 +12,27 @@
 """
 __author__ = 'Liangz'
 
-
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+# from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required, DataRequired
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
 
 
 class NameForm(FlaskForm):
