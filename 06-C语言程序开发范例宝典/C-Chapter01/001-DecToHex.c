7563ceff57048a955f924389f69827dcbbcfa470
Asura <Liangz.org@gmail.com>
2019-09-17 23:41:26 +0800

    study C

---
+++ 06-C语言程序开发范例宝典/C-Chapter01/001-DecToHex.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+/*
+实例 001 十进制转换为十六进制
+*/
+
+void main()
+{
+    int i;
+    printf("Please Input decimalism number: ");
+    scanf("%d", &i);
+    printf("the hex number is 0x%x", i);
+    getchar();
+    getchar();
+}
