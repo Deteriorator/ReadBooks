a2aa1c8552e84c44653ca15df65a51c5dd07cd1c
Asura <Liangz.org@gmail.com>
2019-09-18 22:26:11 +0800

    fix 001 example

--- 06-C语言程序开发范例宝典/C-Chapter01/001-DecToHex.c
+++ 06-C语言程序开发范例宝典/C-Chapter01/001-DecToHex.c
@@ -2,15 +2,70 @@
 #include<stdio.h>
 
 /*
-实例 001 十进制转换为十六进制
-*/
+ *实例 001 十进制转换为十六进制
+ */
+int DecToBin(int x);
 
 void main()
 {
     int i;
     printf("Please Input decimalism number: ");
     scanf("%d", &i);
-    printf("the hex number is 0x%x", i);
+    /*
+     * 直接使用控制字符串
+     * %o 八进制
+     * %x %X 十六进制
+     */
+    printf("the hex number is 0x%x\n", i);
+    printf("the oct number is %o\n", i);
+    printf("the bin number is %d\n", DecToBin(i));
     getchar();
     getchar();
 }
+
+// 十进制转二进制
+long DecToBin(int x)
+{
+    int remainder, temp;    //remainder 是余数
+    long result = 0, k = 1;
+    temp = x;
+    while (temp)
+    {
+        remainder = temp % 2;
+        result = remainder * k + result;
+        k = k * 10;
+        temp = temp / 2;
+    }
+    return result;
+}
+
+
+/*
+函数 char *itoa(int value, char *string, int radix)
+   返回值类型char
+   参数value 待转换的数字
+   参数string 转换后存储到string中
+   参数radix 转换到几进制
+定义在 stdlib.h
+
+示例：
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+int main()
+{
+    int userinput;
+    printf("Please enter a integer.\n");
+    scanf("%d",&userinput);
+
+    char octal[MAX],hex[MAX];
+    itoa(userinput,octal,8);
+    itoa(userinput,hex,16);
+
+    printf("Octal and Hex of the integer %d that you entered is %s and %s.\n",userinput,octal,hex);
+
+    return 0;
+}
+
+*/
